500cc179c039506f9d8710405a9ad6080d35330d
Slice Wess <ernestwesson86@gmail.com>
2020-11-20 19:02:19 -0800

    trying to create state functions for the user search activity

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Search from "../components/Search";
 import Results from "../components/Results";
+// import { useState } from 'react';
 
 function SearchPage () {
     return (
@@ -12,3 +13,35 @@ function SearchPage () {
 }
 
 export default SearchPage;
+
+
+class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
