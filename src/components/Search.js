cc276183764cac61b72e4023120d8494a4321beb
Slice Wess <ernestwesson86@gmail.com>
2020-11-21 14:28:40 -0800

    adding return function

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,10 +20,49 @@ class Search extends Component {
         })
     };
 
-   
+    handleFormSubmit=event => {
+        event.preventDefault();
+        if (this.state.currentTerm) {
+            let localSearchTerms = this.state.currentTerm.split(" ");
+            let queryTerms = localSearchTerms.join("+");
+            let localQueryString = this.state.queryString+queryTerms;
+            API.googleBooks(localQueryString).then(response => {
+                console.log(response)
+                this.setState({
+                    books : response.data.items
+                })
+                console.log(this.state.books)
+            })
+        }
     }
 
-   
+    saveBook = id => {
+        const localBook = this.state.books[id].volumeInfo;
+        const data = {
+            title : localBook.title,
+            authors: localBook.authors,
+            summary: localBook.description,
+            image: localBook.imageLinks.thumbnail,
+            link: localBook.infoLink
+        }
+        API.saveBook(data)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+        alert("Book saved!")
+    }
+
+    render() {
+        return (
+            <div className="col-md-10 mx-auto text-center">
+                <div className="row">
+                    <form className="col-md-6 mx-auto">
+                        
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
 
 // // console.log(books);
 // function Search () {
